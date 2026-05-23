c0050722e1b6b1c8776d0d87e9b5fc979e42d8f8
vamshiganesh <136454745+vamshiganesh@users.noreply.github.com>
2026-05-23 12:02:27 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,26 @@
+class MinStack {
+public:
+    MinStack() {
+        stack<int> stack;
+        stack<int> minStack;
+    }
+    
+    void push(int val) {
+        stack.push(val);
+        val = min(val, minStack.top() if !minStack.empty() else val);
+        minStack.push(val);
+    }
+    
+    void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        return stack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
