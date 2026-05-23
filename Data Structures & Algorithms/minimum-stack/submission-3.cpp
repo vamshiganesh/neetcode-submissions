963273a633223ee274c738d2fa7c55bd598347a7
vamshiganesh <136454745+vamshiganesh@users.noreply.github.com>
2026-05-23 12:08:08 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+class MinStack {
+
+private:
+    std::stack<int> stack;
+    std::stack<int> minStack;
+
+public:
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        stack.push(val);
+        val = min(val, minStack.empty() ? val : minStack.top());
+        minStack.push(val);
+    }
+    
+    void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        return stack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
